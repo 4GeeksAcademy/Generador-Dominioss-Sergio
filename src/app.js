c28e8019db75dc9961bdfe4c11614a69c8653ed8
padyy23 <spadillam23@gmail.com>
2025-05-05 09:22:53 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-import "bootstrap";
-import "./style.css";
+const pronouns = ['the', 'our'];
+const adjectives = ['great', 'big'];
+const nouns = ['jogger', 'racoon'];
 
+const tldSelectEl = document.getElementById('tldSelect');
+const generateButtonEl = document.getElementById('generateButton');
+const domainListEl = document.getElementById('domainList');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateDomainNames = (tld) => {
+  const domains = [];
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        domains.push(`${pronoun}${adjective}${noun}${tld}`);
+      }
+    }
+  }
+  return domains;
 };
+
+const renderDomainList = (domains) => {
+  domainListEl.innerHTML = ''; 
+  domains.forEach(domain => {
+    const listItem = document.createElement('li');
+    listItem.textContent = domain;
+    domainListEl.appendChild(listItem);
+  });
+};
+
+const onGenerateButtonClick = () => {
+  const selectedTld = tldSelectEl.value;
+  const generatedDomains = generateDomainNames(selectedTld);
+  renderDomainList(generatedDomains);
+};
+
+generateButtonEl.addEventListener('click', onGenerateButtonClick);
